0a0fea7e328220f2717060f2abe28b025007f964
Tadele01 <tadeleyednkachw@gmail.com>
2020-03-05 15:58:13 -0500

    show database method and gitignore added

--- main.cpp
+++ main.cpp
@@ -3,13 +3,26 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<unistd.h>
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
-
+const char* WORKING_DIR = "/home/tade/Documents/databases";
 void working_dir_mover(){
-	string working_dir = "/home/tade/Documents/databases";
-	chdir(working_dir);
+	
+	chdir(WORKING_DIR);
 }
+void show_databases(){
+	DIR *dir = opendir(WORKING_DIR);
+	struct dirent *entry = readdir(dir);
+	while (entry != NULL){
+		if(entry->d_type == DT_DIR)
+			printf("%s\n", entry->d_name);
+		entry = readdir(dir);
+	}
+	closedir(dir);
+}
+
+
 
 string space_remover(string sql_query){
 	string temp;
@@ -39,13 +52,15 @@ int main(){
 	cout << "TQL..\n\n";
 	cout << ">> ";
 	while(getline(cin, sql_query)){
-		string formatted_sql_query = input_formatter(sql_query);	
+		string formatted_sql_query = input_formatter(sql_query);
+		boost::trim_right(formatted_sql_query);
 		if(formatted_sql_query=="exit"){
-			cout<<"\n\nGood bye...\n";
+			cout << "Good Bye \n";
 			break;
 		}
 		else if(formatted_sql_query!=""){
 			working_dir_mover();
+			cout << get_current_dir_name() << endl; 
 		}
 		cout<< "\n>> ";
 	}
